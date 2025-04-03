d5da0930b3bc232a57cfdc24a6dc27ec3e57f879
Sanskruti Mhatre <78200050+SanskrutiMhatre@users.noreply.github.com>
2025-04-03 16:37:58 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AppProvider } from "@/context/AppContext";
+import { useEffect } from "React"
 
 // Pages
 import Index from "./pages/Index";
@@ -22,9 +23,25 @@ import ImageDetails from "./pages/ImageDetails";
 import LabsList from "./pages/LabsList";
 import StudentDocumentList from "./pages/StudentDocumentList";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient
 
 const App = () => (
+  useEffect(() => {
+    localStorage.setItem({
+         "dockerImages": [
+        {
+            "semester": "4",
+            "subject": "Networking Lab",
+            "pullCommand": "sudo docker pull 192.168.104.2:5000/networking-lab",
+            "runCommand": "xhost +  sudo docker run --rm -it     --name networking-lab     --net=host     --cap-add=NET_ADMIN     --cap-add=NET_RAW     --cap-add=SYS_ADMIN     --cap-add=SYS_PTRACE     -v /var/run/docker.sock:/var/run/docker.sock     -v /tmp/.X11-unix:/tmp/.X11-unix     -v $HOME/kathara_labs:/home/netuser/kathara_labs     -e DISPLAY=$DISPLAY     -e PULSE_SERVER=unix:${XDG_RUNTIME_DIR}/pulse/native     -v ${XDG_RUNTIME_DIR}/pulse/native:${XDG_RUNTIME_DIR}/pulse/native     --privileged     --user root     192.168.104.2:5000/networking-lab /bin/bash",
+            "instructions": "",
+            "notes": "",
+            "id": "tq35xzdze"
+        }
+    ],
+    })
+  }, [])
+  
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
       <AppProvider>
